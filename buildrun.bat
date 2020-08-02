f488286ee10ca496b8aea4922ccd630ca3fa17f5
Mikael Chudinov <mikael@chudinov.net>
2020-08-02 18:42:28 +0200

    Update to version 7.1.1

--- buildrun.bat
+++ buildrun.bat
@@ -1,2 +1,2 @@
-docker build --build-arg IMAGE_VERSION=7.0.0 -t chudinov/grafana .
+docker build --build-arg IMAGE_VERSION=7.1.1 -t chudinov/grafana .
 docker run -d -it -p 3000:3000 --name grafana chudinov/grafana
